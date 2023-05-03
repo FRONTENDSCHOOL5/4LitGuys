65fde4c2fb72772e87d2f496e686fb4b6e4e06a1
yejilee0714 <ellie.yejilee@gmail.com>
2023-05-04 00:41:36 +0900

    [메인페이지] >> 반응형 추가

--- src/components/main_ss.js
+++ src/components/main_ss.js
@@ -1,13 +1,13 @@
 function Main_ss() {
     return (
         // <h1>Hello world</h1>
-        <section class="section_ss">
-            <div class="info">
-                <p class="info_ss">spent most of his time with his grandmother in Provence, was inspired by the local values of the region. Miro's poems, Provençal ceramic works, cherry trees, nostalgic hand-knitted tablecloths are among these sources of inspiration. The colour palette, dominated by white, blue and earthy tones, harmonised perfectly with the wheat fields.</p>
-                <button type="button" class="btn_ss">SHOP</button>
+        <section className="section_ss">
+            <div className="info">
+                <p className="info_ss">spent most of his time with his grandmother in Provence, was inspired by the local values of the region. Miro's poems, Provençal ceramic works, cherry trees, nostalgic hand-knitted tablecloths are among these sources of inspiration. The colour palette, dominated by white, blue and earthy tones, harmonised perfectly with the wheat fields.</p>
+                <button type="button" className="btn_ss">SHOP</button>
             </div>
-            <div class="image">
-                <img src={require("../image/main_collection1.png")} alt='test-img' />
+            <div className="image">
+                <img src={require("../image/main_collection1.png")}  alt='test-img' />
             </div>
         </section>
     );
