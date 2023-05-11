e32d411bbaae09bcd3f5fbe94d62c83ade4b147f
yejilee0714 <ellie.yejilee@gmail.com>
2023-05-12 00:18:57 +0900

    [메인페이지] >> ss파트 CSS 수정

--- src/components/MainSs.jsx
+++ src/components/MainSs.jsx
@@ -1,3 +1,4 @@
+import '../styles/reset.css';
 import '../styles/mainss.css';
 
 function MainSs() {
@@ -5,6 +6,7 @@ function MainSs() {
         // <h1>Hello world</h1>
         <section className="section_ss">
             <div className="info">
+                <h4>Collection</h4>
                 <p className="info_ss">spent most of his time with his grandmother in Provence, was inspired by the local values of the region. Miro's poems, Provençal ceramic works, cherry trees, nostalgic hand-knitted tablecloths are among these sources of inspiration. The colour palette, dominated by white, blue and earthy tones, harmonised perfectly with the wheat fields.</p>
                 <button type="button" className="btn_ss">SHOP</button>
             </div>
